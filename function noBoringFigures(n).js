169e089c7fa6ad945fe3b533fd2d1c393ddfdc7b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-02 22:19:44 -0700

    1450 -> 145 960000 -> 96 1050 -> 105 -1050 -> -105

--- function noBoringFigures(n).js
+++ function noBoringFigures(n).js
@@ -5,3 +5,12 @@ function noBoringZeros(n) {
     return n;
 }
 
+function noBoringZeros(n) {
+    if (n == 0) return n;
+    else{
+        while ( n % 10 == 0){
+
+            n  /= 10 ;}
+    }
+    return n;
+}
