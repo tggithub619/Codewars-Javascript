1e34c4d2ed6180af3db920f7ffc60461ea2e24f8
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-06 22:59:26 -0700

    https://www.codewars.com/kata/55b051fac50a3292a9000025/train/javascript
    (FilterString("123"), 123, 'Just return the numbers');

--- var FilterString = function(value).js
+++ var FilterString = function(value).js
@@ -1,6 +1,6 @@
 var FilterString = function(value) {
     let str = "";
-    for (let i = 0; i < value.length; i++){
+    for ( let i = 0; i < value.length; i++){
         if (isNaN(value[i]) === false){
             str = str + value[i];
         }
