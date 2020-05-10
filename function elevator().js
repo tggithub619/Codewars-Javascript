118d50f39f29873c92bfebc56fdc51268d3412b4
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-09 21:11:36 -0700

    https://www.codewars.com/kata/5c374b346a5d0f77af500a5a/train/javascript
    elevator(0, 1, 0); // => "left"
    elevator(0, 1, 1); // => "right"
    elevator(0, 1, 2); // => "right"
    elevator(0, 0, 0); // => "right"
    elevator(0, 2, 1); // => "right"

--- function elevator().js
+++ function elevator().js
@@ -2,3 +2,5 @@ function elevator(left, right, call){
     if (Math.abs(left -call) < Math.abs(right - call)) return "left";
     else return "right"
 }
+
+const elevator = (left, right, call) => Math.abs(call-left) < Math.abs(call-right) ? 'left' : 'right'
