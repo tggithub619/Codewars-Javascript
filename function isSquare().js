f312febb870dd4a0540e24b6f0322f7600efd188
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-10 16:23:07 -0700

    https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
    isSquare(-1) returns  false
    isSquare(0) returns   true
    isSquare(3) returns   false
    isSquare(4) returns   true
    isSquare(25) returns  true
    isSquare(26) returns  false

--- function isSquare().js
+++ function isSquare().js
@@ -1,3 +1,10 @@
 function isSquare(n) {
     return Math.sqrt(n) % 1 === 0;
 }
+
+var isSquare = function(n){
+    if(n === 0){
+        return true} else {
+        return n > 0 && Math.sqrt(n) % 1 === 0;
+    }
+}
