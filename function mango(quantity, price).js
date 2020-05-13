5488630f5abb49b370fea84497d3f89c3e68b215
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-13 12:14:39 -0700

    https://www.codewars.com/kata/57a77726bb9944d000000b06

--- function mango(quantity, price).js
+++ function mango(quantity, price).js
@@ -4,24 +4,20 @@ function mango(quantity, price){
     return price * (quantity - Math.floor(quantity / 3));
 }
 
-yesterday
-Refactor
 
 function mango(quantity, price){
     return (Math.floor(quantity/3)*2* price) + (quantity % 3 * price);
 
 }
 
-yesterday
-Refactor
+
 
 function mango(quantity, price){
     return ((quantity-(Math.floor(quantity/3)))*price);
 
 }
 
-yesterday
-Refactor
+
 
 function mango(quantity, price){
     return Math.floor(quantity/3)*price*2+(quantity-3*Math.floor(quantity/3))*price;
