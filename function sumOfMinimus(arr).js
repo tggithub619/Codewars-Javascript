ee0ee4fe359e86af4d52b873e7c4c38e7b3eaa16
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-17 20:22:15 -0700

    https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/train/javascript

--- function sumOfMinimus(arr).js
+++ function sumOfMinimus(arr).js
@@ -16,3 +16,12 @@ function sumOfMinimums(arr) {
 }
 console.log(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]));
 
+function sumOfMinimums(arr) {
+    let total = 0;
+
+    for (let i = 0; i < arr.length; i++){
+        total += Math.min(...arr[i])
+    }
+
+    return total;
+}
