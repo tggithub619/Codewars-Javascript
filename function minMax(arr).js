124e71fc3f1bd4afb329b2788dfde856f4e538aa
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-17 19:58:35 -0700

    https://www.codewars.com/kata/559590633066759614000063/train/javascript

---
+++ function minMax(arr).js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/559590633066759614000063/train/javascript
+
+function minMax(arr){
+    let max = 0;
+    let min = 0;
+    for (let i = 0; i < arr.length; i++){
+        max = Math.max(...arr);
+        min = Math.min(...arr);
+    }
+    return [min, max]; // fix me!
+}
