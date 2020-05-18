409442484e12ac5ca4674b20450729dd498a3644
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-18 13:43:47 -0700

    https://www.codewars.com/kata/562e98755e9214cd2500003d/solutions/javascript/me/best_practice

---
+++ function yourFutureCareer().js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/562e98755e9214cd2500003d/solutions/javascript/me/best_practice
+
+function yourFutureCareer(){
+    var career;
+    if (Math.random(career) <= 0.32)
+        return  "FrontEnd Developer";
+    else if (Math.random(career) <= 0.65)
+        return  'Full-Stack Developer';
+    else
+        return "BackEnd Developer";
+}
