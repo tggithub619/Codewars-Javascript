4bc68f7f4b9c510d1fd89a7b741ab595bdd47660
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-20 23:33:40 -0700

    https://www.codewars.com/kata/5601c5f6ba804403c7000004/train/javascript

---
+++ function barTriangle.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/5601c5f6ba804403c7000004/train/javascript
+
+function barTriang(p1, p2, p3){
+    return [+((p1[0]+p2[0]+p3[0])/3).toFixed(4), +((p1[1]+p2[1]+p3[1])/3).toFixed(4)];
+}
