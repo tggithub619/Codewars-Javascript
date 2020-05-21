795fcc2ec8472db928619a6fca22be03bcb9cd45
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-20 23:23:36 -0700

    https://www.codewars.com/kata/5890d8bc9f0f422cf200006b/train/javascript

---
+++ function excludeingVatPrice(price).js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5890d8bc9f0f422cf200006b/train/javascript
+
+//return price without vat
+function excludingVatPrice(price){
+    return (price === null) ? -1: +(price - (price / 115) * 15).toFixed(2);
+}
