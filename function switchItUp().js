948d82978f3b16815480d29d727f2171c4fbb919
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-24 15:33:49 -0700

    https://www.codewars.com/kata/5808dcb8f0ed42ae34000031

---
+++ function switchItUp().js
@@ -0,0 +1,33 @@
+//https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+function switchItUp(number){
+    let arr = ["Zero", 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+    return arr[number];
+}
+
+function switchItUp(number){
+    switch(number){
+
+        case 1: return 'One';
+            break;
+        case 2: return 'Two';
+            break;
+        case 3: return 'Three';
+            break;
+        case 4: return 'Four';
+            break;
+        case 5: return 'Five';
+            break;
+        case 6: return 'Six';
+            break;
+        case 7: return 'Seven';
+            break;
+        case 8: return 'Eight';
+            break;
+        case 9: return 'Nine';
+            break;
+        case 0: return 'Zero';
+            break;
+
+    }
+}
