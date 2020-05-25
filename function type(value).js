c03bb45521eec6272be418b811025773494f01a0
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-25 15:20:14 -0700

    https://www.codewars.com/kata/57293671c98f77e84b000065/solutions/javascript

--- function type(value).js
+++ function type(value).js
@@ -7,3 +7,22 @@ function type(value) {
     if (value instanceof Object) return 'object'
     return typeof value
 }
+
+function type(value) {
+    if ({}.toString.call(value) === "[object Array]") return 'array';
+    if ({}.toString.call(value) === "[object Object]") return 'object';
+    if ({}.toString.call(value) === "[object Number]") return 'number';
+    if ({}.toString.call(value) === "[object String]") return 'string';
+    if ({}.toString.call(value) === "[object Date]") return 'date';
+    if ({}.toString.call(value) === "[object Null]") return 'null';
+    if ({}.toString.call(value) === "[object Undefined]") return 'undefined';
+}
+
+function type(value) {
+    let str = {}.toString.call(value);
+    let res = '';
+    for (let i = 8; i < str.length - 1; i++){
+        res = res + str[i];
+    }
+    return res.toLowerCase();
+}
