ddde69972b3ed3edec8b0267a4b1633f86a8ad1e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-25 15:46:21 -0700

    https://www.codewars.com/kata/582c81d982a0a65424000201/train/javascript

---
+++ function arrCheck().js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/582c81d982a0a65424000201/train/javascript
+
+function arrCheck(arr){
+    let count = 0;
+    for (let i = 0; i <arr.length; i++){
+        if (Array.isArray(arr[i])) count++;
+    }
+    return count === arr.length
+}
