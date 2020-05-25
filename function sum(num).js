c8088023feea2aea973e72a73209a887a4e39180
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-25 12:42:56 -0700

    https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript

---
+++ function sum(num).js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/53dc54212259ed3d4f00071c/train/javascript
+
+function sum (num) {
+    let s = 0;
+    for (let i = 0; i<num.length; i++){
+        s = s + num[i];
+    }
+    return s;
+
+};
