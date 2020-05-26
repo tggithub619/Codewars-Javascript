af2b92c4e632c28c808aeda45b62470fbfc59cda
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-26 15:50:06 -0700

    https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript

---
+++ function cutIt(arr).js
@@ -0,0 +1,31 @@
+//https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+function cutIt(arr){
+    let len = [];
+    for ( let i = 0; i< arr.length; i++){
+        len.push(arr[i].length);
+    }
+    let min = Math.min(...len);
+    for ( let i = 0; i<arr.length; i++){
+        arr[i] = arr[i].slice(0, min);
+    }
+    return arr;
+}
+
+
+function cutIt(arr){
+    let len = arr.map(el => el.length);
+    let min = Math.min(...len);
+    return arr.map(el => el.slice(0, min));
+
+}
+
+function cutIt(arr){
+    let len = [];
+    for ( let i = 0; i< arr.length; i++){
+        len.push(arr[i].length);
+    }
+    let min = Math.min(...len);
+    return arr.map(el => el.slice(0, min))
+
+}
