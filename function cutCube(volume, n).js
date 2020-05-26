ee54c4753f6d2e99af5e609d53beffe3dbbe0152
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-26 15:52:50 -0700

    https://www.codewars.com/kata/5733f948d780e27df6000e33/train/javascript

---
+++ function cutCube(volume, n).js
@@ -0,0 +1,23 @@
+//https://www.codewars.com/kata/5733f948d780e27df6000e33/train/javascript
+
+function cutCube(volume,n){
+
+
+    if(Number.isInteger(Math.cbrt(volume/n)) && Number.isInteger(Math.cbrt(n))
+    ) return true;
+    else return false;
+
+}
+
+function cutCube(volume,n){
+
+    if(Number.isInteger(Math.cbrt(volume/n)) && Number.isInteger(Math.cbrt(n))) return true;
+    else return false;
+
+
+}
+
+function cutCube(volume,n){
+    if (Math.cbrt(n) % 1 !== 0) return false;
+    else return (Math.cbrt(volume / n) % 1 == 0) ? true : false;
+}
