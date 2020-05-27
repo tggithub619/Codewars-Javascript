2a952c5a59fef9c9097e4313331ccb98d24c23b6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-27 16:01:59 -0700

    https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript

---
+++ function countSheeps(arrayOfSheep).js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/54edbc7200b811e956000556/solutions/javascript
+
+function countSheeps(arrayOfSheep) {
+    let num=0;
+    for(var i=0; i<arrayOfSheep.length; i++)    {
+        if(arrayOfSheep[i]==true)
+            num++;
+    }
+    return num;
+}
+
+function countSheeps(arr) {
+    return arr.filter(Boolean).length;
+}
