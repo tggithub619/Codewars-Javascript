d7b8e4ada2f887e0f8785002cd3ad2f793c6650f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-05-29 21:17:03 -0700

    https://www.codewars.com/kata/56a946cd7bd95ccab2000055/solutions/javascript/following/best_practices

---
+++ function lowercaseCount(str).js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/56a946cd7bd95ccab2000055/solutions/javascript/me/best_practice
+
+function lowercaseCount(str){
+    let arr = str.match(/[a-z]/g);
+    return arr ? arr.length : 0;
+}
+
+function lowercaseCount(str){
+    let count = 0;
+    for (let i = 0; i < str.length; i++){
+        if (str[i] === str[i].toLowerCase()
+            && str[i] != str[i].toUpperCase()
+        ) count ++;
+    }
+    return count;
+}
+
