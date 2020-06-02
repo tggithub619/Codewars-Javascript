69bc27faabdb8e511bf2d15ca544f890fe31e51c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-02 15:13:04 -0700

    https://www.codewars.com/kata/5a04133e32b8b998dc000089/solutions/javascript/me/best_practice

--- function solve(arr).js
+++ function solve(arr).js
@@ -3,3 +3,15 @@
 function solve(arr){
     return arr.filter((el, i) => arr.slice(i + 1).every(x => x < el));
 };
+
+function solve(arr){
+    let temp = [];
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[i] <= arr[j]) count++;
+        }
+        if (count === 0) temp.push(arr[i]);
+    }
+    return temp;
+};
