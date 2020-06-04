2596888196f234aa74eeec7a3d1b9456f7e16184
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-04 13:34:31 -0700

    https://www.codewars.com/kata/57ab2d6072292dbf7c000039/solutions/javascript/me/best_practice

---
+++ function correctPolishLetters(string).js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/57ab2d6072292dbf7c000039/solutions/javascript/me/best_practice
+
+function correctPolishLetters (string) {
+    let obj = {
+        ą: 'a',
+        ć: 'c',
+        ę: 'e',
+        ł: 'l',
+        ń: 'n',
+        ó: 'o',
+        ś: 's',
+        ź: 'z',
+        ż: 'z',
+    }
+    let res = '';
+    for(let el of string){
+        if(obj[el]) res += obj[el]
+        else res += el
+    }
+    return res;
+}
