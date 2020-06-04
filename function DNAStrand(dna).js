6a890df18722b107c7b7048148d4e7d9bb98f2e7
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-04 13:17:45 -0700

    https://www.codewars.com/kata/5a04133e32b8b998dc000089/solutions/javascript/me/best_practice

---
+++ function DNAStrand(dna).js
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/javascript
+
+function DNAStrand(dna){
+    let result = "";
+    for ( let i = 0; i<dna.length; i++){
+        if (dna[i] === "A") {
+            result += "T";}
+        if (dna[i] === "T") {
+            result += "A";}
+        if (dna[i] === "C") {
+            result += "G";}
+        if (dna[i] === "G") {
+            result += "C";}
+    }
+    return result;
+
+}
