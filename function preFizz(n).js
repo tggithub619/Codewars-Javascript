9153d443c19f2b1e338ac260d9a60b4f8604f1fc
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-06 18:12:04 -0700

    https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript

---
+++ function preFizz(n).js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+
+function preFizz(n) {
+    let arr = [];
+    for ( let i = 1; i <= n; i++)
+        arr.push(i);
+    return arr;
+}
