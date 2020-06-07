0fb8c3f9cfbd94d25a6a8c2f0940a069b80820aa
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-06 17:02:11 -0700

    https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/javascript

---
+++ function divisors(integer).js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/javascript
+
+function divisors(integer) {
+    let arr = [];
+    for (i = 2; i <integer; i++){
+        if (integer % i === 0) {
+            arr.push(i);
+        }
+    }
+    if (arr.length === 0) return integer + " is prime";
+    else return arr;
+};
