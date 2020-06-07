c988b77aa4a6da7a43902607d9bb0ef07d7235d8
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-06 17:46:41 -0700

    https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript

---
+++ function countBy(x, n).js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/javascript
+
+function countBy(x, n) {
+    var arr = [];
+    for (let i = 1; i <= n * x; i++) {
+        if (i % x === 0) arr.push(i);
+        return arr;
+    }
