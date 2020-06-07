f21181c98bd75b7ffc1c1a8ae24ca16e938c7e71
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-07 09:37:53 -0700

    https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript

---
+++ function pickIt(arr).js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
+
+function pickIt(arr){
+    var odd=[],even=[], res= [];
+    for ( let i = 0; i < arr.length; i++){
+        if (arr[i]%2 === 0) even.push(arr[i]);
+        if (arr[i]%2 !== 0) odd.push(arr[i]);
+
+    }
+    return [odd,even];
+}
