cde1e53dea99319a22fc22cd50fbdf35fd7ceb34
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-06 18:04:55 -0700

    https://www.codewars.com/kata/55eca815d0d20962e1000106/train/javascript

---
+++ function generateRange(min, max, step).js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/55eca815d0d20962e1000106/train/javascript
+
+function generateRange(min, max, step){
+    let arr = [];
+    for ( let i = min; i <= max; i+=step)
+        arr.push(i);
+    return arr;
+
+}
