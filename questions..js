88287c42284ca8d0c378bb1bd8c9a2bb80a1ce43
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-10 13:55:51 -0700

    https://www.codewars.com/kata/54e8c3e89e2ae6f4900005a1/solutions/javascript/following/best_practice

---
+++ questions..js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/54e8c3e89e2ae6f4900005a1/solutions/javascript/following/best_practice
+
+questions.forEach(function (i) {
+    i.usersAnswer = null;
+});
+
+for(let i = 0; i < questions.length; i++) {
+    questions[i].usersAnswer = null;
+}
