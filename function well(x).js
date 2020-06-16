c3324f14486fe9441bf0a4c66c429ca8afe55c8c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-15 17:31:32 -0700

    https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript

---
+++ function well(x).js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript
+
+function well(x){
+    let res = x.filter(el => el === 'good').length;
+    if (res > 2) return 'I smell a series!'
+    if (res > 0) return 'Publish!'
+    return 'Fail!'
+}
