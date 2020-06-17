cfc859effa29b235bf3ee3b020a979af1d8f7559
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-17 12:00:54 -0700

    https://www.codewars.com/kata/581b30af1ef8ee6aea0015b9/train/javascript

--- function countWins(winnerList, country).js
+++ function countWins(winnerList, country).js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/581b30af1ef8ee6aea0015b9/train/javascript
 
 function countWins(winnerList, country) {
-    return winnerList.filter(el => el.country === country).length
+    return winnerList.filter(el => el.country === country).lengthж
 }
