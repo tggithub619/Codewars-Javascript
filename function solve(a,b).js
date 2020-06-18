1d538435eb5c47a37559ad209ed7a26bad2cb33e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-18 12:12:43 -0700

    https://www.codewars.com/kata/581b30af1ef8ee6aea0015b9/train/javascript

--- function solve(a,b).js
+++ function solve(a,b).js
@@ -17,8 +17,8 @@ function solve(a, b) {
     let sumA = 0;
     let sumB = 0;
     for ( let i = 0; i < 3; i++ ) {
-        if ( a[i] > b[i]) sumA++
-        else if ( a[i] < b[i]) sumB++
+        if ( a[i] > b[i]) sumA++;
+        else if ( a[i] < b[i]) sumB++;
     }
     if (sumA > sumB) return `${sumA}, ${sumB}: Alice made "Kurt" proud!`
     else if (sumA < sumB) return  `${sumA}, ${sumB}: Bob made "Jeff" proud!`
