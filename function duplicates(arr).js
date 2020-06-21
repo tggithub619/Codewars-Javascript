b0d2803d7c9f8c6a8dd032a7a0ba6db9c97af7b3
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-21 12:08:55 -0700

    https://www.codewars.com/kata/5558cc216a7a231ac9000022/train/javascript

---
+++ function duplicates(arr).js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5558cc216a7a231ac9000022/train/javascript
+
+function duplicates(arr) {
+    const duplicates = arr.filter((el, i) => i !== arr.indexOf(el) && i === arr.lastIndexOf(el));
+    return duplicates
+}
