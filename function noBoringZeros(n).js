27ca26bca62123599a163345e65b1b4c78f17953
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-06-24 13:56:36 -0700

    https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript

--- function noBoringZeros(n).js
+++ function noBoringZeros(n).js
@@ -14,3 +14,15 @@ function noBoringZeros(n) {
     }
     return n;
 }
+
+function noBoringZeros(n) {
+    let str = n + '';
+    let newStr = str.split("");
+    for(let i = newStr.length - 1; i >= 0; i--){
+        if(newStr[i] === "0")
+            newStr.pop();
+        else break;
+    }
+
+    return +newStr.join('');
+}
