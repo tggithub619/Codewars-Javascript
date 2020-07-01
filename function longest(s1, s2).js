deff72683dbe79977fdeb175bb82e59f1cc9c504
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-01 14:21:33 -0700

    https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript

---
+++ function longest(s1, s2).js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+
+function longest(s1, s2) {
+    return  [...new Set(s1+s2)].sort().join('')
+}
+
+function longest(s1, s2) {
+    let res = (s1 + s2);
+    let arr = res.split('')
+//   let arr= [];
+//   for ( let i = 0; i< res.length; i++){
+//     arr.push(res[i])
+//   }
+    arr = arr.filter((el, i) => arr.indexOf(el) === i).sort().join('')
+
+    // let str = '';
+    //   for (let i = 0; i < arr.length; i++){
+//     str = str +arr[i]
+//   }
+    return arr;
+}
