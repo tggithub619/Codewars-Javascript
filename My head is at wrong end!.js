1f3defff2b48a39e28ef0a25360f6e0ba768beb1
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-07 19:06:10 -0700

    Update My head is at wrong end!.js

--- My head is at wrong end!.js
+++ My head is at wrong end!.js
@@ -1,14 +1,13 @@
 //https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/javascript
 
 function fixTheMeerkat(arr) {
-    return arr.reverse()
+    return arr.reverse();
 }
 
 function fixTheMeerkat(arr) {
     //your code here
     var newarr=[];
-    for(var i=arr.length-1;i>=0;i--)
-    {
+    for(var i=arr.length-1;i>=0;i--){
         newarr.push(arr[i]);
     }
     return newarr;
