e04cbe57b3a5bea3266d133371cc73ce1f54d3e6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-13 16:03:05 -0700

    https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/javascript

--- function sumOfDifferences(arr) {.js
+++ function sumOfDifferences(arr) {.js
@@ -15,3 +15,22 @@ function sumOfDifferences(arr) {
     }
     return first + second;
 }
+
+function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+}
+
+function sumOfDifferences(arr) {
+    return arr.sort((a, b) => b - a)
+        .map((a, i) => a - arr[i + 1] || 0)
+        .reduce((a, b) => a + b, 0);
+}
+
+function sumOfDifferences(arr) {
+    arr.sort((a, b) => a - b);
+    let b = 0;
+    for(let a = arr.length - 1; a > 0; a--) {
+        b += arr[a] - arr[a - 1];
+    }
+    return b;
+}
