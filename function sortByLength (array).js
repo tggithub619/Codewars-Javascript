09c7fe042c70b34c8c3e56681bbb3fa9a6b59283
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-13 16:07:15 -0700

    https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/javascript

--- function sortByLength (array).js
+++ function sortByLength (array).js
@@ -3,3 +3,19 @@
 function sortByLength (array) {
     return array.sort((a,b)=>a.length-b.length)
 };
+
+
+function sortByLength (array) {
+    for (let i = 0; i < array.length; i++) {
+        let minIdx = i;
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[minIdx].length > array[j].length) {
+                minIdx = j;
+            }
+        }
+        let temp = array[i];
+        array[i] = array[minIdx];
+        array[minIdx] = temp;
+    }
+    return array;
+}
