3afb82bebb8215f96661b98383a5d7d91e47b8f2
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-12 23:15:05 -0700

    https://www.codewars.com/kata/5731861d05d14d6f50000626/train/javascript

--- function bigToSmall(arr).js
+++ function bigToSmall(arr).js
@@ -3,3 +3,11 @@
 function bigToSmall(arr){
     return [].concat(...arr).sort((a,b)=>b-a).join('>');
 }
+
+function bigToSmall(arr){
+    let result = [];
+    for (let i = 0; i<arr.length; i++) {
+        result=result.concat(arr[i])}
+    let sorted = result.sort((a,b)=>b-a).join('>')
+    return sorted;
+}
