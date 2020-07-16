6f84b4ac86b681c2463e0b31490f47ef076e08cb
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-16 13:16:25 -0700

    https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript

--- Sentence Smash.js
+++ Sentence Smash.js
@@ -4,3 +4,14 @@
 function smash (words) {
     return words.join(' ')
 };
+
+function smash (words) {
+    let str = '';
+    for ( let i =0; i <words.length; i++){
+        if(i != words.length-1){
+            words[i]= words[i]+ ' '
+        }
+        str+= words[i]
+    }
+    return str
+};
