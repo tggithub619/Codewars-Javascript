93c898b83a2178a649dc85b50cdcb361a1cf368e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-17 13:04:55 -0700

    https://www.codewars.com/kata/54598d1fcbae2ae05200112c/train/javascript

--- True for All.js
+++ True for All.js
@@ -3,3 +3,11 @@
 function all( arr, fun ){
     return arr.every(fun);
 }
+
+function all(arr, fun){
+    for (let i=0; i<arr.length; ++i){
+        if (!fun(arr[i]))
+            return false;
+    }
+    return true;
+}
