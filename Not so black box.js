4be7cf7db3b215100c84edec325613b3e555fb26
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-17 22:40:29 -0700

    https://www.codewars.com/kata/57080f21d531cd94950007eb/train/javascript

---
+++ Not so black box.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/57080f21d531cd94950007eb/train/javascript
+
+JSopenSesame();
+
+theBoxHasBeenOpened ="XD"
+theBoxHasBeenOpenedTheProperWay=":)"
+
+
+theBoxHasBeenOpenedTheProperWay=":)"
+
+
+
