c21c434c172b82bbe369ac3f47ceb3a719e5a7da
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-19 16:01:41 -0700

    https://www.codewars.com/kata/57080f21d531cd94950007eb/train/javascript

--- Find Maximum and Minimum Values of a List.js
+++ Find Maximum and Minimum Values of a List.js
@@ -1,12 +1,12 @@
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
 var min = function(list){
-    list.sort((a,b)=> a-b)
+    list.sort((a,b)=> a-b);
     return list[0];
 }
 
 var max = function(list){
-    list.sort((a,b)=> b-a)
+    list.sort((a,b)=> b-a);
     return list[0];
 }
 
