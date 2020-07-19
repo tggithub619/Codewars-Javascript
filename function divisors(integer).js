fa4b3a5575cc815ccc786bbb0df84b9e11d1295c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-19 16:03:47 -0700

    https://www.codewars.com/kata/57080f21d531cd94950007eb/train/javascript

--- function divisors(integer).js
+++ function divisors(integer).js
@@ -2,17 +2,16 @@
 
 function divisors(integer) {
     let arr = [];
-    for (i = 2; i <integer; i++){
+    for (let i = 2; i <integer; i++){
         if (integer % i === 0) {
             arr.push(i);
         }
     }
     if (arr.length === 0) return integer + " is prime";
     else return arr;
-
 };
 
 function divisors(integer) {
-    for(var div = [], i = 2; i < integer; i++) if(integer % i == 0) div.push(i);
+    for(let div = [], i = 2; i < integer; i++) if(integer % i == 0) div.push(i);
     return div.length > 0 ? div : integer + " is prime";
 }
