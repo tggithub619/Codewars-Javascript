3cce2d5d1c7a2686dbd1e8784513500023180200
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-19 16:02:43 -0700

    https://www.codewars.com/kata/57080f21d531cd94950007eb/train/javascript

--- function createArray(number).js
+++ function createArray(number).js
@@ -1,11 +1,9 @@
 //https://www.codewars.com/kata/55c28f7304e3eaebef0000da/train/javascript
 
 function createArray(number){
-    var newArray = [];
-
-    for(var counter = 1; counter <= number; counter++){
-        newArray.push(counter);
+   let newArray = [];
+    for(let i = 1; i <= number; i++){
+        newArray.push(i);
     }
-
     return newArray;
 }
