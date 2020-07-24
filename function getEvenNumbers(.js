7afce58a7ad2eba9745cd3757e87497c1adfb20e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-23 18:34:59 -0700

    https://www.codewars.com/kata/558f0553803bc3c4720000af/train/javascript
    add loop solution

--- function getEvenNumbers(.js
+++ function getEvenNumbers(.js
@@ -3,3 +3,12 @@
 function getEvenNumbers(arr){
     return arr.filter(el => el % 2 ===0)
 }
+
+function getEvenNumbers(num){
+    let arr = []
+    for ( let i = 0; i < num.length; i++){
+        if (num[i]%2 ==0)
+            arr.push(num[i])
+    }
+    return arr;
+}
