bfc3952f70b41fd0e0f5e7d33bcd588e69c39118
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-23 18:42:01 -0700

    added nested loop

--- Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js
+++ Find The Duplicated Number in a Consecutive Unsorted List - Tougher Version.js
@@ -3,3 +3,13 @@
 var findDup=function(arr){
     return arr.filter((el, i) => i === arr.indexOf(el) && i !== arr.lastIndexOf(el))[0]
 }
+
+function findDup(arr){
+    for (let i=0; i<arr.length; ++i){
+        for (let j=i+1; j<arr.length; ++j){
+            if (arr[i]==arr[j])
+                return arr[i]
+        }
+    }
+    return 0;
+}
