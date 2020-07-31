69b1bdd9825cb8ff70c4ea6635b934a2b06099f3
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-31 15:27:11 -0700

    https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript

--- First non-repeating character.js
+++ First non-repeating character.js
@@ -2,8 +2,9 @@
 
 function firstNonRepeatingLetter(str) {
     let newStr = str.toLowerCase();
-    for (let i = 0; i < newStr.length; i++)
+    for (let i = 0; i < newStr.length; i++) {
         if (newStr.indexOf(newStr[i]) === newStr.lastIndexOf(newStr[i]))
             return str[i];
+    }
     return '';
 }
