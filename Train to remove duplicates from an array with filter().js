0dae8340af633616eab169631fa0fed450b9b395
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-31 16:06:35 -0700

    https://www.codewars.com/kata/58308360aeb69a460b0002b2/train/javascript

---
+++ Train to remove duplicates from an array with filter().js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/58308360aeb69a460b0002b2/train/javascript
+
+function unique(arr) {
+    let res =  arr.filter((el, i) => arr.indexOf(el) === i);
+    return res
+}
+function unique(arr) {
+    return arr.filter((el, i) => arr.indexOf(el) === i);
+//[...new Set(arr)]
+}
