84e6a8750ab36406fa8224eb8711c40e32e38925
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-07-31 16:05:27 -0700

    https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript

--- List Filtering.js
+++ List Filtering.js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
 
 function filter_list(l) {
-    return l.filter(el => typeof(el)  === 'number')
+    return l.filter(el => typeof(el)  === 'number');
 }
