46726fa6a5ac2f5dc0fb4fe7061cedb62053b28a
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-06 19:34:29 -0700

    https://www.codewars.com/kata/5ba178be875de960a6000187/solutions/javascript/me/best_practice

--- Multiples By Permutations II.js
+++ Multiples By Permutations II.js
@@ -8,3 +8,15 @@ function findLowestInt(k) {
      }
     return n;
 }
+
+function findLowestInt(k) {
+    let n = 1
+    while (true){
+        let n1 = k * n;
+        let n2 = (k + 1) * n;
+        let s1 = (n1+"").split("").sort().join("");
+        let s2 = (n2+"").split("").sort().join("");
+        if (s1 === s2) return n;
+        n += 1
+    }
+}
