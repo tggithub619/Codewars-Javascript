8e9a21d4c0908e88299274f19b07446383fc118b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-08 23:45:24 -0700

    https://www.codewars.com/kata/57256064856584bc47000611/train/javascript

--- function howManySmaller(arr,n).js
+++ function howManySmaller(arr,n).js
@@ -6,4 +6,11 @@ function howManySmaller(arr,n){
 
 }
 
+function howManySmaller(arr,n){
+    let newArr =[]
+    for (let i = 0; i < arr.length; i++){
+        if ( arr[i].toFixed(2)<n) newArr.push(arr[i])
+    }
+    return newArr.length;
+}
 
