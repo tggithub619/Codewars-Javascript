fc7465bba8f5a516b0854dceb59d500eec2b9ef3
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-08 22:58:47 -0700

    https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript

---
+++ Array plus array.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/javascript
+
+function arrayPlusArray(arr1, arr2) {
+    let newArr = arr1.concat(arr2)
+    return newArr.reduce((a,b)=> a+b)
+}
+
+function arrayPlusArray(arr1, arr2) {
+    let sum1 = 0;
+    let sum2 = 0;
+    for (let i =0; i< arr1.length; i++){
+        sum1+= arr1[i]
+    }
+    for (let i =0; i< arr2.length; i++){
+        sum2+= arr2[i]
+    }
+    return sum1+sum2;
+}
