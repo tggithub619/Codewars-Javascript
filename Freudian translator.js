73eb273b4516c795eaa019084d2092518f8e26a3
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-10 14:44:16 -0700

    https://www.codewars.com/kata/57256064856584bc47000611/train/javascript

--- Freudian translator.js
+++ Freudian translator.js
@@ -9,3 +9,15 @@ function toFreud(str) {
     return str.split(" ").map((el)=> el === ""?"":"sex").join(" ");
 }
 
+function toFreud(string) {
+    if (string.length === 0) return '';
+    let str = '';
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === ' ') count++;
+    }
+    for (let i = 1; i <= count+1; i++) {
+        str +=  i !== count + 1 ? 'sex ' : 'sex';
+    }
+    return str;
+}
