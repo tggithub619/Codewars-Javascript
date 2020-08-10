f63aca96039229ea0a1d1a94be7a35be3609469f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-10 14:41:55 -0700

    https://www.codewars.com/kata/57256064856584bc47000611/train/javascript

--- Training JS #16.js
+++ Training JS #16.js
@@ -4,3 +4,31 @@ function cutIt(arr){
     let length = Math.min(...arr.map(el => el.length))
     return arr.map(el=> el.slice(0, length))
 }
+
+function cutIt(arr){
+    let len = [];
+    for ( let i = 0; i< arr.length; i++){
+        len.push(arr[i].length);
+    }
+    let min = Math.min(...len);
+    for ( let i = 0; i<arr.length; i++){
+        arr[i] = arr[i].slice(0, min);
+    }
+    return arr;
+}
+
+function cutIt(arr){
+    let len = arr.map(el => el.length);
+    let min = Math.min(...len);
+    return arr.map(el => el.slice(0, min));
+
+}
+function cutIt(arr){
+    let len = [];
+    for ( let i = 0; i< arr.length; i++){
+        len.push(arr[i].length);
+    }
+    let min = Math.min(...len);
+    return arr.map(el => el.slice(0, min))
+
+}
