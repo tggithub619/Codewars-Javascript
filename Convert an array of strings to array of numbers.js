32413f228c11d6d7875c518574a04f1013776b77
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-10 14:58:33 -0700

    https://www.codewars.com/kata/57256064856584bc47000611/train/javascript

--- Convert an array of strings to array of numbers.js
+++ Convert an array of strings to array of numbers.js
@@ -3,3 +3,19 @@
 function toNumberArray(stringarray){
     return stringarray.map(el=> +el)
 }
+
+function toNumberArray(stringarray){
+    return stringarray.map(Number);
+}
+
+function toNumberArray(stringarray){
+    return stringarray.map(parseFloat);
+}
+
+function toNumberArray(str){
+    let arr = [];
+    for(let i = 0; i < str.length; i++){
+        arr.push(+str[i]);
+    }
+    return arr;
+}
