1028d46518e342b3d56a12a7bd3cda5c85b9e46f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-10 18:28:59 -0700

    https://www.codewars.com/kata/57256064856584bc47000611/train/javascript

--- Beginner - Lost Without a Map.js
+++ Beginner - Lost Without a Map.js
@@ -3,3 +3,11 @@
 function maps(x){
     return x.map(el=> el*2)
 }
+
+function maps(x){
+    let arr =[]
+    for (let i = 0; i < x.length; i++) {
+        arr.push(x[i]*2)
+    }
+    return arr
+}
