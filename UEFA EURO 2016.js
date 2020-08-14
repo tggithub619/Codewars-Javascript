c8cb6450b77f47c50ab9d15b957970c41f468e52
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-14 12:29:52 -0700

    2

--- UEFA EURO 2016.js
+++ UEFA EURO 2016.js
@@ -13,4 +13,6 @@ function uefaEuro2016(teams, scores){
     return `At match ${teams.slice(0,1)} - ${teams.slice(1)}, ${str}`
 }
 
-`At match ${c[0]} - ${c[1]}, ${s[0] == s[1] ? "commands played draw." : (s[0] > s[1] ? c[0] : c[1]) + ' won!'}`;
+function uefaEuro2016(teams, scores){
+    return `At match ${teams[0]} - ${teams[1]}, ${scores[0] == scores[1] ? "teams played draw." : (scores[0] > scores[1] ? teams[0] : teams[1]) + ' won!'}`;
+}
