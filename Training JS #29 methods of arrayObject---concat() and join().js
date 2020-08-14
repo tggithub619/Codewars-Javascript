4e692120147051a167d20d6871cc864a04d860e1
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-14 12:58:06 -0700

    1

---
+++ Training JS #29 methods of arrayObject---concat() and join().js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5731861d05d14d6f50000626/solutions/javascript
+
+function bigToSmall(arr) {
+    return [].concat(...arr).sort((a, b) => b - a).join('>');
+}
+
+function bigToSmall(arr){
+    let res = [];
+    for (let i = 0; i<arr.length; i++) {
+        res=res.concat(arr[i]);
+    }
+    res = res.sort((a,b)=>b-a).join('>');
+    return res;
+}
