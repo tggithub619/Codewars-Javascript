1823cdb0cf8c6d9b400e45e6038184f208c55653
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-13 19:34:00 -0700

    3

--- Ironman Triathlon.js
+++ Ironman Triathlon.js
@@ -22,3 +22,13 @@ function iTri(s){
 function iTri(s){
     return (s >= 140.6) ? `You're done! Stop running!` : s ? {[(s > 114.4) ? `Run` : (s > 2.4) ? `Bike` : `Swim`]: (s > 130.6) ? `Nearly there!` : (140.6 - s).toFixed(2) + ` to go!`} : `Starting Line... Good Luck!`;
 }
+
+function iTri(s) {
+    let d = 2.4 + 112 + 26.2;
+    if (s === 0) return 'Starting Line... Good Luck!';
+    if (s <= 2.4) return {'Swim': `${(d - s).toFixed(2)} to go!`};
+    if (s <= 114.4) return {'Bike': `${(d - s).toFixed(2)} to go!`};
+    if (d - s > 10) return {'Run': `${(d - s).toFixed(2)} to go!`};
+    if (s < d) return {'Run': 'Nearly there!'};
+    else return "You're done! Stop running!";
+}
