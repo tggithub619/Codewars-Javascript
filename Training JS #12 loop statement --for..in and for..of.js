6ab929f3d7501fc426071fecdfe19b1e03cf6729
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-14 12:28:17 -0700

    1

---
+++ Training JS #12 loop statement --for..in and for..of.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
+
+function giveMeFive(obj){
+    let arr = [];
+    for (let key in obj){
+        if(key.length == 5) arr.push(key)
+        if(obj[key].length === 5) arr.push(obj[key])
+    }
+    return arr;
+}
+
+function giveMeFive(obj){
+    let keys = Object.entries(obj).flat()
+    let values = Object.values(obj)
+    let arr =  keys.concat(values)
+    return arr.filter(el => el.length == 5)
+    // for let key in obj
+}
