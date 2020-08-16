db1382eec93230ab090b6c8258d4a739724df572
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-16 15:49:48 -0700

    2

--- Short Long Short.js
+++ Short Long Short.js
@@ -4,3 +4,7 @@ function solution(a, b){
     if ( a.length < b.length){return a + b + a;
     } else return b + a + b
 }
+
+function solution(a, b){
+    return (a.length<b.length)? a+b+a : b+a+b
+}
