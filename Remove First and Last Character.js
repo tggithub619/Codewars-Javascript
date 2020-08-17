f82c61baf29c98d0a990cebe832e39bf5bd0c4df
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-17 14:27:26 -0700

    3

--- Remove First and Last Character.js
+++ Remove First and Last Character.js
@@ -9,3 +9,11 @@ function removeChar(str){
     return str.split('').filter((el, i) => i >=1 && i < str.length-1).join('')
 };
 
+function removeChar(str){
+    let res = "";
+    for (let i = 1; i <str.length -1; i++){
+        res = res + str[i];
+    }
+    return res;
+}
+
