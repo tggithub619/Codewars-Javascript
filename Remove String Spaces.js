bd1943c234d115eff39c90219736b7eb02d55b26
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-17 14:39:40 -0700

    2

---
+++ Remove String Spaces.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/javascript
+
+function noSpace(x){
+    return x.split(' ').join('');
+}
+
+function noSpace(x){
+    let res = ''
+    for ( let i = 0; i < x.length; i++){
+        if (x[i] !== ' '){
+            res+=x[i]
+        }
+    }
+    return res
+}
