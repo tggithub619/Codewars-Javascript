0f45047a7f5a5129a95f98c4ade97f7f7021b467
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-17 13:45:48 -0700

    1

--- 5 without numbers.js
+++ 5 without numbers.js
@@ -1,5 +1,6 @@
 //https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
 
+
 function unusualFive() {
     return 'table'.length
 }
---
+++ Unique In Order.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript
+
+var uniqueInOrder=function(str){
+    let arr = [];
+    for (let i =0; i < str.length; i++){
+        if ( str[i] !== str[i+1])
+            arr.push(str[i])
+    }
+    return arr
+}
