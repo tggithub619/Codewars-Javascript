de594f2062dae0c6167f95bdc0d4c377286edf24
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-17 21:29:48 -0700

    1

---
+++ Reversed Strings.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/javascript
+
+function solution(str){
+    let res = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        res = res + str[i];
+    }
+    return res;
+}
