bd97cc5b5eafeba659889b623f5d369d1bfe40b1
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-17 21:58:19 -0700

    2

--- Is it a palindrome.js
+++ Is it a palindrome.js
@@ -4,3 +4,12 @@ function isPalindrome(x) {
     let y = x.toLowerCase().split('').reverse().join('');
     return x.toLowerCase() === y
 }
+
+function isPalindrome(x) {
+    x = x.toLowerCase();
+    let y = '';
+    for (let i = x.length - 1; i >= 0; i--) {
+        y= y +x[i];
+    }
+    return x === y.toLowerCase()
+}
