6b104ebc2fe042cb32c5d1e1f5c39a3414a2ce1f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-19 13:57:10 -0700

    2

--- Thinking & Testing Something capitalized.js
+++ Thinking & Testing Something capitalized.js
@@ -2,4 +2,20 @@
 
 function testit(s){
     return s.toUpperCase();
+    }
+
+function testit(s){
+    let str = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i+1] === ' ' || i === s.length-1) {
+            str += s[i].toUpperCase();
+        } else {
+            str += s[i]
+        }
+    }
+    return str;
+}
+
+function testit(s) {
+    return (s.length == 0) ? '' : s.split(' ').map(el => el.slice(0, -1) + el[el.length-1].toUpperCase()).join(' ');
 }
