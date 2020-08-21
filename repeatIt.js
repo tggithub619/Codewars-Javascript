01eed638cbeb0f7d49235eac7f30d37fe0e44c1f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-21 15:28:11 -0700

    2

--- repeatIt.js
+++ repeatIt.js
@@ -3,3 +3,15 @@
 var repeatIt = function(str, n) {
     return (typeof str === 'string') ? str.repeat(n): "Not a string"
 }
+
+var repeatIt = function(str, n) {
+    let res = ''
+    let i = 1
+    while (i<=n){
+        if (typeof(str) === "string"){
+            res += str;
+            i++
+        }
+    }
+    return res
+}
