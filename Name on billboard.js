8df7f1904c9a3602efc8adabce369aa954a08405
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-21 15:52:12 -0700

    1

---
+++ Name on billboard.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/570e8ec4127ad143660001fd/train/javascript
+
+function billboard(name, price = 30){
+    let res =0;
+    let i =0;
+    while ( i < name.length){
+        res+= price;
+        i++;
+    }
+    return res
+}
