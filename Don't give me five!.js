cd97b759be9b9059aeb7fb9559efbc04c79331d6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-23 21:55:25 -0700

    2

---
+++ Don't give me five!.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5813d19765d81c592200001a/train/javascript
+
+function dontGiveMeFive(start, end){
+    let arr = [];
+    for(let i=start; i<=end; i++){
+        if(i.toString().includes('5') == false) {
+            arr.push(i);
+        }
+    }
+    return arr.length;
+}
