b4fb0a07b21f510e5c913966e09fa83cd3fff3ea
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-24 19:11:50 -0700

    1

---
+++ Find the position!.js
@@ -0,0 +1,6 @@
+//https://www.codewars.com/kata/5808e2006b65bff35500008f/train/javascript
+
+function position(letter) {
+    let alph = ' abcdefghijklmnopqrstuvwxyz';
+    return `Position of alphabet: ${alph.indexOf(letter)}`;
+}
