c19838e13ab1fc4905641e1225b6dd83480b9591
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-26 10:50:47 -0700

    4

--- validate code with simple regex.js
+++ validate code with simple regex.js
@@ -4,3 +4,21 @@ function validateCode(code) {
     code= code + '';
     return (code.startsWith('1') || code.startsWith('2') || code.startsWith('3')) ? true : false;
 }
+
+function validateCode(code) {
+    code= code + '';
+    if (code.startsWith('1') || code.startsWith('2') || code.startsWith('3')){
+        return true;
+    }
+    return  false;
+}
+
+
+function validateCode(code) {
+    code = '' +code;
+    return (code[0]<4)? true : false
+}
+
+function validateCode (code) {
+    return (code.toString().startsWith(1) || code.toString().startsWith(2) ||code.toString().startsWith(3)) === true
+}
