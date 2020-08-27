c41d976129120e3019355ad63a8fe9f0e7f02aee
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-27 10:56:33 -0700

    4

--- Credit Card Mask.js
+++ Credit Card Mask.js
@@ -3,3 +3,26 @@
 function maskify(cc) {
     return cc.slice(0, -4).replace(/\w/g, "#") + cc.slice(-4);
 }
+
+function maskify(cc) {
+    cc = cc.split('');
+    for(let i = 0; i<cc.length-4; i++){
+        cc[i] = "#";
+    }
+    cc = cc.join("");
+    return cc
+}
+
+function maskify(cc) {
+    cc = cc.split('');
+    let part1 =[];
+    for(let i = 0; i<cc.length-4; i++){
+        cc[i] = "#";
+        part1.push(cc[i])
+    }
+    return part1.concat(cc.slice(-4)).join('')
+}
+
+function maskify(cc) {
+    return '#'.repeat(cc.slice(0, -4).length) + cc.slice(-4);
+}
