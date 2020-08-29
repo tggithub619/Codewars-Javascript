cf1fdc363dd81358a6777d09ae386eda79b7b4b5
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-28 19:59:23 -0700

    2

--- Remove the time.js
+++ Remove the time.js
@@ -1,7 +1,7 @@
 //https://www.codewars.com/kata/56b0ff16d4aa33e5bb00008e/train/javascript
 
 function shortenToDate(longDate) {
-    return longDate.split(',')[0]
+    return longDate.split(',')[0];
 }
 
 function shortenToDate(longDate) {
