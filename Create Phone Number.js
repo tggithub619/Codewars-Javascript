418ea86ebf9bd9f9c28303a24737b7417e578b2d
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-28 20:01:18 -0700

    2

--- Create Phone Number.js
+++ Create Phone Number.js
@@ -1,17 +1,14 @@
 //https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
 
 function createPhoneNumber(num){
-    return '(' + num.join('').slice(0, 3)+') ' + num.join('').slice(3, 6)+'-'+num.join('').slice(6)
+    return '(' + num.join('').slice(0, 3)+') ' + num.join('').slice(3, 6)+'-'+num.join('').slice(6);
 }
 
 function createPhoneNumber(numbers){
     var format = "(xxx) xxx-xxxx";
-
-    for(var i = 0; i < numbers.length; i++)
-    {
+    for(var i = 0; i < numbers.length; i++)   {
         format = format.replace('x', numbers[i]);
     }
-
     return format;
 }
 
