0ebf0ffe6132c6a170246347fc8b0afb90218825
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-30 14:30:01 -0700

    2

--- Reversed Words.js
+++ Reversed Words.js
@@ -3,3 +3,12 @@
 function reverseWords(str){
     return str.split(' ').reverse().join(' ')
 }
+
+function reverseWords(str){
+    let res = [];
+    str = str.split(" ");
+    for(let i=str.length-1; i>=0; i--){
+        res.push(str[i]);
+    }
+    return res.join(" ")
+}
