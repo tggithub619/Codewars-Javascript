24a8c37ea319c2d63cc12622146f8323109fde83
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-29 21:41:47 -0700

    1

--- Automorphic Number (Special Numbers Series 6).js
+++ Automorphic Number (Special Numbers Series 6).js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/javascript
 
 function automorphic(n){
-    return ((Math.pow(n,2)+'').endsWith(n))? "Automorphic" : "Not!!"
+    return ((Math.pow(n,2)+'').endsWith(n))? "Automorphic" : "Not!!";
 }
