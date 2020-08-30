e5326b319bdc89f0e80e3fc4bf3ffb38e6d89a2d
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-30 13:00:35 -0700

    1

--- Contamination 1 -String.js
+++ Contamination 1 -String.js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/596fba44963025c878000039/train/javascript
 
 function contamination(text, char){
-    return text.replace(/./g, char)
+    return text.replace(/./g, char);
 }
