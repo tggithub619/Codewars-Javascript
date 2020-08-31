4112737cd211b28d39461c120bcaf7a5f951f95b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-08-30 20:00:10 -0700

    1

--- Descending Order.js
+++ Descending Order.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
 
 function descendingOrder(n){
-    return  +n.toString().split('').reverse().join('')
+    return  +n.toString().split('').sort().reverse().join('')
 
 }
