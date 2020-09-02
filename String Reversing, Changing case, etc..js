39ecb3ec08383a0fdd2a83d1199906a0ed3bae83
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-01 18:39:40 -0700

    2

--- String Reversing, Changing case, etc..js
+++ String Reversing, Changing case, etc..js
@@ -11,3 +11,15 @@ function withoutReverseMirror(str){
 function reverseAndMirror(s1,s2) {
     return`${withReverseMirror(s2)}@@@${withReverseMirror(s1)}${withoutReverseMirror(s1)}`;
 }
+
+function reverseAndMirror(s1,s2) {
+    let strOne = s1.replace(/[A-z]/g, val=> val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase());
+    let strTwo = s2.replace(/[A-z]/g, val=> val === val.toLowerCase() ? val.toUpperCase() : val.toLowerCase());
+    let str1 = strOne.split('').reverse();
+    let str2 = strTwo.split('').reverse();
+    let str = str1.join('');
+    let newStr = str.concat(strOne);
+    let strSecond = str2.join('') + '@@@';
+
+    return strSecond.concat(newStr);
+}
