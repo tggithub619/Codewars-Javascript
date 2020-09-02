bfd466d8ae82fe5fd1d41b340f137aaa967fa39f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-01 18:37:47 -0700

    1

---
+++ String Reversing, Changing case, etc..js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/58305403aeb69a460b00019a/train/javascript
+
+function withReverseMirror(str) {
+    return  [...str].reverse().map(el => el === el.toLowerCase() ? el.toUpperCase() : el.toLowerCase()).join('')
+}
+
+function withoutReverseMirror(str){
+    return [...str].map(el => el === el.toLowerCase() ? el.toUpperCase() : el.toLowerCase()).join('')
+}
+
+function reverseAndMirror(s1,s2) {
+    return`${withReverseMirror(s2)}@@@${withReverseMirror(s1)}${withoutReverseMirror(s1)}`
+}
