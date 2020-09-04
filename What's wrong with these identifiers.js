5b815fdb9dc1b0cd520b417f4bc4ccea4651cf96
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-03 21:02:30 -0700

    1

---
+++ What's wrong with these identifiers.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/56bb01de0e8b29de50000b19/train/javascript
+
+var Person = {
+    "1stname": "John",
+    "second-name": "Doe",
+    "email@ddress": "john.doe@email.com",
+    "male.female": "M"
+};
