7d7cce3203d7293273ba89da995b3af9071ffd92
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-03 20:36:42 -0700

    1

--- Sentences should start with capital letters..js
+++ Sentences should start with capital letters..js
@@ -1,5 +1,12 @@
 //https://www.codewars.com/kata/5bf774a81505a7413400006a/train/javascript
 
 function fix(p){
-    return p.split('. ').map( w =>  w.substring(0,1).toUpperCase()+ w.substring(1)).join('. ')
+    return p.split('. ').map( w =>  w.substring(0,1).toUpperCase()+ w.substring(1)).join('. ');
 }
+
+function fix(p){
+    return (p.length==0)? '':p
+        .split('. ')
+        .map((el) => el[0].toUpperCase() + el.slice(1))
+        .join('. ');
+};
