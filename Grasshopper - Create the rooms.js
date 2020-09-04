b36714e9c7ebf3ada4c738d16b33e88827f947a7
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-03 20:54:07 -0700

    1

---
+++ Grasshopper - Create the rooms.js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+var rooms = {
+    room1:{
+        "name": "name1",
+        "description": "description1",
+        "completed": "completed1"
+    },
+    room2: {
+        "name": "name2",
+        "description": "description2",
+        "completed": "completed2"
+    },
+    room3: {
+        "name": "name3",
+        "description": "description3",
+        "completed": "completed3"
+    }
+
+}
