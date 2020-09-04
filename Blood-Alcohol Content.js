669136eb0bf51f2284237b105756a62414add6b6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-03 22:17:14 -0700

    1

---
+++ Blood-Alcohol Content.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/571b6a4a7beb0a8ade0007a8/train/javascript
+
+function bloodAlcoholContent(drinks, weight, sex, time){
+    if (sex === 'female') {
+        sex = 0.66
+    }   else if (sex === 'male'){
+        sex = 0.73
+    }
+    let bac = ((drinks.ounces * drinks.abv)* 5.14 / weight * sex) - 0.015 * time
+    return +bac.toFixed(4);
+}
