803ca1f538eb4a61103309e53a49c0f89adba307
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-04 22:24:20 -0700

    2

--- function type(value).js
+++ function type(value).js
@@ -4,7 +4,7 @@ function type(value) {
     if (value instanceof Array) return 'array';
     if (value instanceof Date) return 'date';
     if (value === null) return 'null';
-    if (value instanceof Object) return 'object'
+    if (value instanceof Object) return 'object';
     return typeof value
 }
 
