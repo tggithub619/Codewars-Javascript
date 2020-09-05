4c3e2e1d9532353854052d9bb9e0662388352fc6
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-04 22:23:48 -0700

    2

--- function type(value).js
+++ function type(value).js
@@ -1,7 +1,7 @@
 //https://www.codewars.com/kata/57293671c98f77e84b000065/solutions/javascript
 
 function type(value) {
-    if (value instanceof Array) return 'array'
+    if (value instanceof Array) return 'array';
     if (value instanceof Date) return 'date'
     if (value === null) return 'null'
     if (value instanceof Object) return 'object'
