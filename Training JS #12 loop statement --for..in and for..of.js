9ab47fe3c217e60c97b5264630188edfdc6e328d
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-05 22:37:56 -0700

    2

--- Training JS #12 loop statement --for..in and for..of.js
+++ Training JS #12 loop statement --for..in and for..of.js
@@ -4,7 +4,7 @@ function giveMeFive(obj){
     let arr = [];
     for (let key in obj){
         if(key.length == 5) arr.push(key);
-        if(obj[key].length === 5) arr.push(obj[key])
+        if(obj[key].length === 5) arr.push(obj[key]);
     }
     return arr;
 }
