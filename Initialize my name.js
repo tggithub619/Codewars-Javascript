e7c4e8e7f1955c1624c38529df48e197fa3f725f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-06 17:33:54 -0700

    2

--- Initialize my name.js
+++ Initialize my name.js
@@ -1,12 +1,12 @@
 //https://www.codewars.com/kata/5768a693a3205e1cc100071f/train/javascript
 
 function initializeNames(name){
-    let arr = name.split(' ')
+    let arr = name.split(' ');
     if( arr.length > 2){
         for (let i = 1; i< arr.length-1; i++){
-            arr[i] =arr[i].slice(0,1)+ '.'
+            arr[i] =arr[i].slice(0,1)+ '.';
         }
     }
 
-    return arr.join(' ')
+    return arr.join(' ');
 }
