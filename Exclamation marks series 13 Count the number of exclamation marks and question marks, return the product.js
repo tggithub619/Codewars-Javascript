ccbd2b90280cbd1033c09a05377c922ac0653540
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-06 17:34:20 -0700

    2

--- Exclamation marks series 13 Count the number of exclamation marks and question marks, return the product.js
+++ Exclamation marks series 13 Count the number of exclamation marks and question marks, return the product.js
@@ -1,8 +1,8 @@
 //https://www.codewars.com/kata/57fb142297e0860073000064/train/javascript
 
 function product(s){
-    let str = s.split('')
-    let excl = str.reduce((c, el) => (el === '!' ? c + 1 : c),0)
-    let que = str.reduce((c, el) => (el === '?' ? c + 1 : c),0)
-    return excl * que
+    let str = s.split('');
+    let excl = str.reduce((c, el) => (el === '!' ? c + 1 : c),0);
+    let que = str.reduce((c, el) => (el === '?' ? c + 1 : c),0);
+    return excl * que;
 }
