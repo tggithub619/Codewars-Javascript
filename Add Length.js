0a9587219706bb9a8c26b236f083f48a91a90ba7
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-07 20:37:26 -0700

    2

--- Add Length.js
+++ Add Length.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/559d2284b5bb6799e9000047/train/javascript
 
 function addLength(str) {
-    let arr =  str.split(' ').map(el=> el+ ' ' + el.length)
+    let arr =  str.split(' ').map(el=> el+ ' ' + el.length);
     return arr
 }
