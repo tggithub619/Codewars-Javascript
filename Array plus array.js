b99aacb3bca97f583ba66e0d909add2894df32fe
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-09 13:33:42 -0700

    2

--- Array plus array.js
+++ Array plus array.js
@@ -9,7 +9,7 @@ function arrayPlusArray(arr1, arr2) {
     let sum1 = 0;
     let sum2 = 0;
     for (let i =0; i< arr1.length; i++){
-        sum1+= arr1[i]
+        sum1+= arr1[i];
     }
     for (let i =0; i< arr2.length; i++){
         sum2+= arr2[i]
