0940f1df15275562ac1a67c7cf76b9dac84a5a5a
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-19 14:36:05 -0700

    2

--- Holy cats.js
+++ Holy cats.js
@@ -9,3 +9,13 @@ function holycats(input){
     return arr;
 }
 
+function holycats(input){
+    let arr =[];
+    for(let i = 0; i < input.length; i++){
+        if(input[i] !== 'wicked' && input[i] !== 'normal'){
+            arr.push(input[i]);
+        }
+    }
+    return arr;
+}
+
