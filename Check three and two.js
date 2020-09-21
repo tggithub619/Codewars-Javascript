07bcd38be2c6fcd0e3b040cba801a3a05cf7c199
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-20 23:07:26 -0700

    3

--- Check three and two.js
+++ Check three and two.js
@@ -9,3 +9,21 @@ function checkThreeAndTwo(arr) {
     }
     return (a==3 || b==3 || c==3 ) && (a==2 || b==2 || c==2)
 }
+
+function checkThreeAndTwo(arr) {
+    let res = [];
+    res.push(arr.filter(x => x === 'a').length)
+    res.push(arr.filter(x => x === 'b').length)
+    res.push(arr.filter(x => x === 'c').length)
+    return res.includes(2) && res.includes(3);
+}
+
+function checkThreeAndTwo(arr) {
+    let obj ={};
+    for (let el of arr){
+        if (obj[el]) obj[el] ++;
+        else obj[el] = 1;
+    }
+    let a = Object.values(obj).filter(el=>el<2 || el >3)
+    return a.length === 0;
+}
