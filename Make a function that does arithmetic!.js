839cd03a4f40efa83109fd2bae902088d8dff33a
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-20 21:39:34 -0700

    1

---
+++ Make a function that does arithmetic!.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+
+function arithmetic(a, b, op){
+    if (op === "add") return a+b;
+    if (op === "subtract") return a-b;
+    if (op === "multiply") return a*b;
+    if (op === "divide") return a/b;
+
+}
