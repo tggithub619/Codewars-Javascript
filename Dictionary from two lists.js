7b4df737049d56b770d48f75ed76b1c7006a7fa7
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-20 23:45:26 -0700

    1

---
+++ Dictionary from two lists.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/5533c2a50c4fea6832000101/train/javascript
+
+function createDict(keys, values){
+    let res = {};
+    for (let i = 0; i < keys.length; i++) {
+        if (i< values.length ){
+            res[keys[i]] = values[i];
+        } else res[keys[i]] = null
+    }
+    return res;
+}
