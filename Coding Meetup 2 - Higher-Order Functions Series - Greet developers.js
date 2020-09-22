cc5a07387c12287ad1dc4f72ef5f1dc1a17797d7
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-22 00:22:21 -0700

    1

---
+++ Coding Meetup 2 - Higher-Order Functions Series - Greet developers.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/58279e13c983ca4a2a00002a/train/javascript
+
+function greetDevelopers(list) {
+    let obj = [];
+    for (let i of list) {
+        i.greeting = `Hi ${i.firstName}, what do you like the most about ${i.language}?`;
+        obj.push(i)
+    }
+    return obj;
+}
