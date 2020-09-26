b80060df47f000552d8efb81461faff71d46a06e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-25 22:06:06 -0700

    1

---
+++ Mirror, mirror, on the wall....js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5f55ecd770692e001484af7d/solutions/javascript
+
+function mirror(data) {
+    let arr = [];
+    for (let el of data){
+        arr.push(el);
+    }
+    arr.sort((a, b) => a - b);
+    let length = arr.length;
+    for (let i = 1; i < length; i++){
+        arr.push(arr[length - 1 - i]);
+    }
+    return arr;
+}
