004a6532fd53db524e463c29e42b299ffe924255
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-28 13:45:13 -0700

    2

--- The Office I - Outed.js
+++ The Office I - Outed.js
@@ -1,7 +1,7 @@
 //https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/train/javascript
 
 function outed(meet, boss) {
-    let teamSize = Object.keys(meet).length
+    let teamSize = Object.keys(meet).length;
     let avgHappiness = Object.values(meet).reduce((a, b) => a + b) + meet[boss]
     return (avgHappiness / teamSize) > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
 }
