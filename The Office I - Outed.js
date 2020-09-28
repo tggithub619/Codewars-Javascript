ce07046b517ef895e394896ec45e06ea3d6f420c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-27 21:51:28 -0700

    2

--- The Office I - Outed.js
+++ The Office I - Outed.js
@@ -6,4 +6,16 @@ function outed(meet, boss) {
     return (avgHappiness / teamSize) > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
 }
 
-
+function outed(meet, boss){
+    var sum=0;
+    var count=0;
+    for (var key in meet)
+        if (key!=boss)
+        { sum+=meet[key];
+            count++;
+        } else {
+            sum+=meet[key]*2;
+            count++;
+        }
+    return sum/count<=5?'Get Out Now!':'Nice Work Champ!';
+}
