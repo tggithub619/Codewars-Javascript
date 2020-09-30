713fa952de2780a00df520fe3e7a1d49cbf25084
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-09-30 12:59:39 -0700

    2

--- String Reordering.js
+++ String Reordering.js
@@ -4,3 +4,18 @@ function sentence(List) {
     let res = List.map(el => Object.entries(el)[0]).sort((a, b) => a[0] - b[0])
     return res.map(el => el[1]).join(' ')
 }
+
+function sentence(List) {
+    let obj = {}
+    let res = ''
+    for (let i = 0; i < List.length; i++) {
+        for (let key in List[i]){
+            obj[key]=List[i][key]
+        }
+    }
+    let arr = Object.entries(obj).map(el => el[0] = +el[0]).sort((a,b) => a-b);
+    for (let i = 0; i < arr.length; i++){
+        res += obj[arr[i]] + ' ';
+    }
+    return res.trim();
+}
