73ae15c4645d0677a9fbabc1e6cba0be8692c58a
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-01 13:30:13 -0700

    2

--- Minimum to multiple.js
+++ Minimum to multiple.js
@@ -3,3 +3,11 @@
 function minimum(a, x) {
     return Math.min(a % x, x - a % x)
 }
+
+const minimum = (a, x) => {
+    for (let i = 0; i < x; i++) {
+        if ((a - i) % x === 0 || (a + i) % x === 0) {
+            return i;
+        }
+    }
+}
