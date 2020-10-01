d8e76c3bfd64094a041a33fe5ab3956dbe2b1a9f
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-01 12:24:09 -0700

    1

---
+++ Maximum Triplet Sum (Array Series 7).js
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/5aa1bcda373c2eb596000112/train/javascript
+
+function maxTriSum(num){
+    let arr1 = []
+    let arr2 = []
+    let a = Math.max(...num)
+    for( let i = 0; i<num.length; i++){
+        if (num[i] !== a){
+            arr1.push(num[i])
+        }
+    }
+    let b = Math.max(...arr1)
+    for( let i = 0; i<arr1.length; i++){
+        if (arr1[i] !== b){
+            arr2.push(arr1[i])
+        }
+    }
+    let c = Math.max(...arr2)
+    return a+b+c
+}
