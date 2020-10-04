705a906dec58f4cc7a96fc9b6686d16564f12479
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-03 23:15:34 -0700

    2

--- Coding Meetup 2 - Higher-Order Functions Series - Greet developers.js
+++ Coding Meetup 2 - Higher-Order Functions Series - Greet developers.js
@@ -4,13 +4,11 @@ function greetDevelopers(list) {
     let obj = [];
     for (let i of list) {
         i.greeting = `Hi ${i.firstName}, what do you like the most about ${i.language}?`;
-        obj.push(i)
+        obj.push(i);
     }
     return obj;
 }
 
 function greetDevelopers(list) {
-    return list.map(
-        x => ({...x, greeting: `Hi ${x.firstName}, what do you like the most about ${x.language}?`})
-    );
+    return list.map(x => ({...x, greeting: `Hi ${x.firstName}, what do you like the most about ${x.language}?`}));
 }
