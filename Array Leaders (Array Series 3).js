0bf08ad1a66c248dacf532c3a654e1e340093994
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-04 17:22:48 -0700

    2

--- Array Leaders (Array Series 3).js
+++ Array Leaders (Array Series 3).js
@@ -1,6 +1,5 @@
 //https://www.codewars.com/kata/5a651865fd56cb55760000e0/train/javascript
 
 var arrayLeaders = num => {
-
     return num.filter((el,i,arr)=>el>arr.slice(i+1).reduce((a,b)=>a+b,0))
 }
