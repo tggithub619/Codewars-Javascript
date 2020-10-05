8ea7203a67e81200ec5e88cd1e9759aeeace2b3b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-04 17:26:09 -0700

    2

--- Array Leaders (Array Series 3).js
+++ Array Leaders (Array Series 3).js
@@ -3,3 +3,13 @@
 var arrayLeaders = num => {
     return num.filter((el,i,arr)=>el>arr.slice(i+1).reduce((a,b)=>a+b,0))
 }
+
+var arrayLeaders = num => {
+    const right = []
+    let s = 0
+    for (let i = num.length - 1; i>= 0;i--){
+        right[i] = s
+        s += num[i]
+    }
+    return num.filter((el, i) => el>right[i])
+}
