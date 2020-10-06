3f21a4672c057508470214cf82060dccef6f1a5e
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-05 22:56:52 -0700

    1

---
+++ lucky number.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/55afed09237df73343000042/train/javascript
+
+function isLucky(n) {
+    let sum = 0
+    let str = n+''
+    for(let i=0; i<str.length; i++){
+        sum += str[i]
+    }
+    return (sum==0 || sum%9==0) ? true : false
+}
+
