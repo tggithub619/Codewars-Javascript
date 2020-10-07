fa3e029744c2d7dcfb54fcca8f3760be37e6d26b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-07 13:26:33 -0700

    2

--- Grains.js
+++ Grains.js
@@ -3,3 +3,12 @@
 function square(number){
     return Math.pow(2, number - 1);
 }
+
+
+function square(number){
+    let res= 1;
+    for(let i = 1; i<number; i++){
+        res *= 2
+    }
+    return res;
+}
