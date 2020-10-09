3433176839e5c7374676ab47cdc257cbfb6d0e51
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-08 19:33:04 -0700

    1

---
+++ Cat years, Dog years.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/javascript
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears = (humanYears===1)? 15:(humanYears == 2)? 15+9: ((humanYears-2)*4)+15+9
+    let dogYears = (humanYears===1)? 15:(humanYears == 2)? 15+9: ((humanYears-2)*5)+15+9
+    return [humanYears, catYears, dogYears];
+}
