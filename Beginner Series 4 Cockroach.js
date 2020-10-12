c17d4eefb196f9a6582f8bdadb38818dc3da2dc4
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-12 14:56:13 -0700

    1

--- Beginner Series 4 Cockroach.js
+++ Beginner Series 4 Cockroach.js
@@ -1,5 +1,5 @@
 //https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
 
 function cockroachSpeed(s) {
-    return Math.trunc(s * 1000 / 36)
+    return Math.trunc(s * 1000 / 36);
 }
