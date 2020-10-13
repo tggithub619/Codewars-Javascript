c62a6d08cf8eec0d337b87e901ee147dfb03dd1c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-13 13:49:20 -0700

    1

---
+++ altERnaTIng cAsE - ALTerNAtiNG CaSe.js
@@ -0,0 +1,5 @@
+//https://www.codewars.com/kata/56efc695740d30f963000557
+
+String.prototype.toAlternatingCase = function () {
+    return this.split("").map(a => a === a.toUpperCase()? a.toLowerCase(): a.toUpperCase()).join('')
+}
