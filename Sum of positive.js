2ba64cead9effe91a652bf7b47bc9ffcf5b5f834
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-13 13:10:25 -0700

    1

---
+++ Sum of positive.js
@@ -0,0 +1,10 @@
+//https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
+
+function positiveSum(arr) {
+    let newArr= []
+    for ( i in arr){
+        if (arr[i]>0) {
+            newArr.push(arr[i])}
+    }
+    return newArr.reduce((a,b)=> a+b, 0)
+}
