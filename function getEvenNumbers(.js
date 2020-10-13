f91e14bfdcaa21210c6b2166e03ad9fe306e665b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-12 20:15:18 -0700

    1

--- function getEvenNumbers(.js
+++ function getEvenNumbers(.js
@@ -1,14 +1,14 @@
 //https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript
 
 function getEvenNumbers(arr){
-    return arr.filter(el => el % 2 ===0)
+    return arr.filter(el => el % 2 ===0)ж
 }
 
 function getEvenNumbers(num){
-    let arr = []
+    let arr = []ж
     for ( let i = 0; i < num.length; i++){
         if (num[i]%2 ==0)
-            arr.push(num[i])
+            arr.push(num[i])ж
     }
     return arr;
 }
