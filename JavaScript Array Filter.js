ae3d01b70c63dfbc380fc93077bc0cb9ef54e0f1
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-14 22:59:55 -0700

    1

--- JavaScript Array Filter.js
+++ JavaScript Array Filter.js
@@ -1,14 +1,14 @@
 //https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript
 
 function getEvenNumbers(arr){
-    return arr.filter(el => el % 2 ===0)
+    return arr.filter(el => el % 2 ===0;
 }
 
 function getEvenNumbers(num){
     let arr = []
     for ( let i = 0; i < num.length; i++){
         if (num[i]%2 ==0)
-            arr.push(num[i])
+            arr.push(num[i]);
     }
     return arr;
 }
