f5007d333da4008e6df47b61967bf768b1ac98e9
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-15 23:50:41 -0700

    2

--- Do you speak English.js
+++ Do you speak English.js
@@ -1,9 +1,9 @@
 //https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058/train/javascript
 
 function spEng(sentence){
-    return sentence.toLowerCase().includes('english')
+    return sentence.toLowerCase().includes('english');
  }
 
 function spEng(sentence) {
-    return sentence.toLowerCase().indexOf('english') !== -1
+    return sentence.toLowerCase().indexOf('english') !== -1;
 }
