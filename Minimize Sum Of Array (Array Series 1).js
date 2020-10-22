168a97da7d5637710e784c66caa6dcbdd102b3ab
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-10-21 23:42:04 -0700

    2

--- Minimize Sum Of Array (Array Series 1).js
+++ Minimize Sum Of Array (Array Series 1).js
@@ -10,3 +10,12 @@ function minSum(arr) {
     }
     return sum;
 }
+
+function minSum(arr) {
+    let sum = 0;
+    arr.sort((a,b) => a-b);
+    for (let i = 0; i < arr.length/2; i++){
+        sum += arr[i] * arr[arr.length-1-i];
+    }
+    return sum;
+}
