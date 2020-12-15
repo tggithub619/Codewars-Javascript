a72f69611f9086209e3c8ca12266786cdcc77e39
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-12-14 20:01:18 -0800

    1

---
+++ Training JS 11 loop statement  break,continue.js
@@ -0,0 +1,14 @@
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b
+
+function grabDoll(dolls){
+    let bag = [];
+    for ( let i=0; i < dolls.length; i++){
+        if (dolls[i] == "Hello Kitty" || dolls[i] == "Barbie doll" ){
+            bag.push(dolls[i]);
+
+        } else continue;
+        if (bag.length >= 3) break;
+    }
+
+    return bag;
+}
