d9a625ce1f491c49a73e30f737c9b878169f1409
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2020-12-20 23:07:42 -0800

    1

---
+++ String doubles.js
@@ -0,0 +1,36 @@
+//https://www.codewars.com/kata/5a145ab08ba9148dd6000094
+
+function doubles(s){
+    let result = []
+    for (let item of s) {
+        if (result.length > 0 && result[result.length - 1] == item) {
+            result.pop()
+        } else {
+            result.push(item)
+        }
+    }
+    return result.join('')
+}
+
+//Test.assertEquals(doubles('abbbzz'),'ab')
+// Test.assertEquals(doubles('zzzzykkkd'),'ykd')
+// Test.assertEquals(doubles('abbcccdddda'),'aca')
+// Test.assertEquals(doubles('vvvvvoiiiiin'),'voin')
+// Test.assertEquals(doubles('rrrmooomqqqqj'),'rmomj')
+// Test.assertEquals(doubles('xxbnnnnnyaaaaam'),'bnyam')
+
+function doubles(s){
+    s1= s.split('')
+    res = []
+    for(let i = 0; i<s1.length; i++){
+        if (s1[i] == s1[i+1]){
+            i+=1
+            continue;
+        }
+        else {
+            res.push(s1[i])
+        }
+    }
+    console.log(s)
+    return res.join('')
+}
