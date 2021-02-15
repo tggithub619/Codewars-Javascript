84c3573ff3e8aa2c5640106f27324719e5e6e2fe
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-14 22:56:07 -0800

    1

---
+++ Nth Root of a Number Redux… Redux!.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/5526a3e41d58a2e980001073
+
+var root = function(x, n){
+    return x **(1/n);
+}
+
+//assertFuzzyEquals(root(4, 2), 2);
+//     assertFuzzyEquals(root(8, 3), 2);
+//     assertFuzzyEquals(root(16,2), 4);
+//     assertFuzzyEquals(root(27,3), 3);
+//     assertFuzzyEquals(root(1e6,6), 10);
+//   });
