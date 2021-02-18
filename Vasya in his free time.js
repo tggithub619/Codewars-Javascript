a50eec0c70d495d10855b164efd9b7ab51a27c0c
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-17 21:03:11 -0800

    1

--- Vasya in his free time.js
+++ Vasya in his free time.js
@@ -7,3 +7,8 @@ function CalculateString(n, nums) {
 
 // Test.assertEquals(CalculateString(4,"1100"),0);
 //   Test.assertEquals(CalculateString(5,"01010"),1);
+
+function CalculateString(n,  nums) {
+    const count = (str, c) => [...str].reduce((a, b) => a + (b == c), 0);
+    return Math.abs(count(nums, '0') - count(nums, '1'));
+}
