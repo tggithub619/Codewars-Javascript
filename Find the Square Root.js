f34c9d8220c0e6441e05ef089791265ab640e71b
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-02-22 10:51:43 -0800

    1

---
+++ Find the Square Root.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/56426b4e971f01fd2f00005b
+
+function squareRoot(x) {
+    return +(x**0.5).toFixed(5)
+}
+
+//Test.assertEquals(squareRoot(81), 9);
+// Test.assertEquals(squareRoot(15), 3.87298);
+// Test.assertEquals(squareRoot(21), 4.58258);
