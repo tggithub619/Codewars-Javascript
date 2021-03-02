e416e5e1c4cb6bc38fda15cce05bf91d6034d075
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-03-01 23:03:01 -0800

    1

---
+++ Valid Parentheses.js
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/52774a314c2333f0a7000688
+
+function validParentheses(parens){
+    let count = 0;
+    for (let i = 0; i < parens.length; i++) {
+        switch (parens[i]) {
+            case "(": count++; break;
+            case ")": count--; break;
+            default: break;
+        }
+        if (count < 0) return false;
+    }
+    return count === 0;}
+
+
+
+//    Test.assertEquals(validParentheses( "()" ), true);
+// Test.assertEquals(validParentheses( "())" ), false);
