c1d06ae9568c0e4dc03192852d98a9d20da6eed5
Tatyana Gamaley <60828574+tggithub619@users.noreply.github.com>
2021-04-04 16:06:48 -0700

    1

--- Char Code Calculation.js
+++ Char Code Calculation.js
@@ -11,3 +11,9 @@ function calc(x){
     return +total1 - +total2
 }
 
+function calc(x){
+    let sum = n => [...n].reduce((a,b) => +a + +b);
+    let total1 = x.replace(/./g, x => x.charCodeAt(0));
+    let total2 = total1.replace(/7/g,'1');
+    return sum(total1) - sum(total2);
+}
